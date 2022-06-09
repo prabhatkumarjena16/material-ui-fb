e2e57928e27ae07f8397043e1773c43dece1fc2e
prabhatkumarjena16 <prabhatkumarjena16@gmail.com>
2022-06-09 19:20:20 +0530

    layout for webpage

---
+++ src/components/Feed.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function Feed() {
+  return (
+    <Box flex={2} p={2} bgcolor="green">
+      Feed
+    </Box>
+  );
+}
+
+export default Feed;
---
+++ src/components/Rightbar.jsx
@@ -0,0 +1,12 @@
+import { Box } from "@mui/material";
+import React from "react";
+
+function Rightbar() {
+  return (
+    <Box flex={1} p={2} bgcolor="orange">
+      Rightbar
+    </Box>
+  );
+}
+
+export default Rightbar;
